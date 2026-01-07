345ccbd71728ce5c8312f5f5baf66825f810ef65
Grey <sujalkumarsahu24@gmail.com>
2026-01-07 10:15:43 +0530

    Time: 5 ms (51.52%) | Memory: 19.5 MB (16.44%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -2,7 +2,6 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         s= str(num)
         l = len(s)-1
-        dic = {'I' : 1, 'V' : 5, 'X' : 10, 'L':50, 'C':100, 'D':500, 'M':1000}
         s = ''
         digits = [int(d) for d in str(num)]
         for d in digits:
