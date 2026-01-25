04f07108c21bad7d8895b13d4be63e925275dd29
Grey <sujalkumarsahu24@gmail.com>
2026-01-25 19:00:09 +0530

    Time: 638 ms (6.22%) | Memory: 7.9 MB (11.58%) - LeetSync

--- 1033-broken-calculator/broken-calculator.cpp
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -4,19 +4,18 @@ public:
         int ops = 0;
         while(target != startValue){
             if(target < startValue){
-                while(target!=startValue){
-                    target++;
-                    ops++;
-                }
-                break;
+                target++;
+                ops++;
             }
             else if(target%2 == 0){
                 target /= 2;
+                ops++;
             }
             else{
                 target++;
+                ops++;
             }
-            ops++;
+            
         }
         return ops;
     }
