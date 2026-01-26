bb25d127748c8e300a17052a8bd02fb904e0cd4a
Grey <sujalkumarsahu24@gmail.com>
2026-01-26 20:49:39 +0530

    Time: 30 ms (61.23%) | Memory: 107.7 MB (27.54%) - LeetSync

--- 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
@@ -6,35 +6,11 @@ public:
             mp[i]++;
         }
         int rounds = 0;
-        for(int i : tasks){
-            if(mp[i] == 2){
-                mp[i]-= 2;
-                rounds+=1;            
-            }
-            if(mp[i] == 3){
-                mp[i]-= 3;
-                rounds+=1;            
-            }
-            if(mp[i] == 4){
-                mp[i]-= 4;
-                rounds+=2;            
-            }
-            if(mp[i] == 5){
-                mp[i]-=5;
-                rounds+=2;
-            }
-            if(mp[i] == 6){
-                mp[i]-= 6;
-                rounds+=2;            
-            }
-            if(mp[i] == 1){
-                return -1; 
-                break;
-            }
-            if(mp[i] > 6){
-                 mp[i]-=3;
-                 rounds++;
-            }
+        for(auto &p:mp){
+             if(p.second==1) return -1;
+             rounds+=p.second/3;
+
+             if(p.second%3!=0) rounds++;
         }
         return rounds;
     }
