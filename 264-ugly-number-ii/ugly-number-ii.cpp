63ed68436cabe6bb3d34237d5d7fa6dde1a838ad
Grey <sujalkumarsahu24@gmail.com>
2026-01-30 21:45:02 +0530

    Time: 4 ms (62.62%) | Memory: 11.3 MB (85.06%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> arr(n+1);
+        vector<int> arr(n);
         arr[0] = 1;
         int i2 = 0, i3 = 0, i5 = 0;
-        for(int i = 1; i<n+1; i++){
+        for(int i = 1; i<n; i++){
             arr[i] = min({2 * arr[i2], 3 * arr[i3], 5 * arr[i5]});
             if(2 * arr[i2] == arr[i]) i2++;
             if(3 * arr[i3] == arr[i]) i3++;
