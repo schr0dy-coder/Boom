899d51426ff83732cd030e5786712d7ab0d5535b
Grey <sujalkumarsahu24@gmail.com>
2026-01-30 21:44:07 +0530

    Time: 7 ms (44.26%) | Memory: 11.4 MB (68.40%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -5,13 +5,10 @@ public:
         arr[0] = 1;
         int i2 = 0, i3 = 0, i5 = 0;
         for(int i = 1; i<n+1; i++){
-            int value2 = 2 * arr[i2];
-            int value3 = 3 * arr[i3];
-            int value5 = 5 * arr[i5];
-            arr[i] = min({value2, value3, value5});
-            if(value2 == arr[i]) i2++;
-            if(value3 == arr[i]) i3++;
-            if(value5 == arr[i]) i5++;
+            arr[i] = min({2 * arr[i2], 3 * arr[i3], 5 * arr[i5]});
+            if(2 * arr[i2] == arr[i]) i2++;
+            if(3 * arr[i3] == arr[i]) i3++;
+            if(5 * arr[i5] == arr[i]) i5++;
         }
         return arr[n-1];
     }
