9582e07e2ad0c67a0ff5fda29dc12fc2a9c19983
Grey <sujalkumarsahu24@gmail.com>
2026-02-01 11:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (96.48%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        int a = 51, b = 51, n = nums.size();
-        for(int i = 1; i < n; i++){
+        int a = 51, b = 51; 
+        for(int i = 1; i < nums.size(); i++){
             if(nums[i] < b){
                 b = nums[i];
                 if(b < a) swap(a, b);
