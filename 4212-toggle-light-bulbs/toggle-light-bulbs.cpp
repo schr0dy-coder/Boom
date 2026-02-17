ee500f4842caa6337c5594d02f0afb135280a096
Grey <sujalkumarsahu24@gmail.com>
2026-02-17 11:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 34.5 MB (48.94%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) {
-        sort(bulbs.begin(), bulbs.end());
-        vector<int> ans;
-        int count = 1;
-        int prev = bulbs[0];
-        for (int i = 1; i < bulbs.size(); i++) {
-            if (bulbs[i] == prev) {
-                count++;
-            } else {
-                if (count % 2 == 1) {
-                    ans.push_back(bulbs[i-1]);
-                }
-                count = 1;
-                prev = bulbs[i];
-            }
-        }
-        if(count%2 == 1)
-            ans.push_back(bulbs[bulbs.size()-1]);
-        return ans;
+    unordered_map<int, int> counts;
+    for (int b : bulbs) counts[b]++;
+    
+    vector<int> ans;
+    for (auto const& [id, freq] : counts) {
+        if (freq % 2 != 0) ans.push_back(id);
     }
+    sort(ans.begin(), ans.end()); 
+    return ans;
+}
 };
